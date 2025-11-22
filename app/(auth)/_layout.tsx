6f17be2f403a47599704bcf989a81e65095d5ca5
SAYANTANI <sayantanipatra089@outlook.com>
2025-11-22 21:44:50 +0530

    Implement Router Setup

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import {Redirect, Slot} from "expo-router";
+
+export default function _Layout() {
+    const isAuthenticated=true;
+    if(!isAuthenticated)return <Redirect href="/sign-in"/>
+    return (
+            <Slot/>
+
+    )
+}
